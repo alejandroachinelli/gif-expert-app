9b72bb03ac289d805c1fe682d5866c4dcd8e35cf
Alejandro Achinelli <alejandromartin.achinelli@gmail.com>
2021-10-25 23:33:29 -0300

    Commit de gif-expert-app

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+export const getGifs = async(category) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=20&api_key=CftjyN0EzAeq2k0BGOa6cllHPrsto44t`;
+    const resp = await fetch(url);
+    const {data} = await resp.json();
+
+    const gifs = data.map( (img) => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_still.url
+        };
+    });
+
+    return gifs;
+}
